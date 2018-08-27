1865ec6692875c212cb35102731bf789ed9b62b5
erikur <erick@actual-training.com>
2018-08-27 13:45:39 +0700

    program kalkulator sederhana

--- SampleAppKelasB/SampleAppKelasB/KalkulatorPage.xaml.cs
+++ SampleAppKelasB/SampleAppKelasB/KalkulatorPage.xaml.cs
@@ -20,7 +20,12 @@ namespace SampleAppKelasB
 
         private void BtnSubmit_Clicked(object sender, EventArgs e)
         {
-            
+            double bil1 = Convert.ToDouble(entryBil1.Text);
+            double bil2 = Convert.ToDouble(entryBil2.Text);
+            double hasil = bil1 + bil2;
+            entryHasil.Text = hasil.ToString();
+            /*DisplayAlert("Keterangan", $"Jadi Hasil {bil1} + {bil2} = {hasil}",
+                "OK");*/
         }
     }
 }
