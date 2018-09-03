e443c065eeeb9e87a6658f4962a65e3e90546a52
erikur <erick@actual-training.com>
2018-09-03 13:09:34 +0700

    menambahkan stepper

--- SampleAppKelasB/SampleAppKelasB/ContohViewPage.xaml.cs
+++ SampleAppKelasB/SampleAppKelasB/ContohViewPage.xaml.cs
@@ -9,12 +9,24 @@ using Xamarin.Forms.Xaml;
 
 namespace SampleAppKelasB
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ContohViewPage : ContentPage
-	{
-		public ContohViewPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContohViewPage : ContentPage
+    {
+        public ContohViewPage()
+        {
+            InitializeComponent();
+            btnGetValue.Clicked += BtnGetValue_Clicked;
+            stepperNilai.ValueChanged += StepperNilai_ValueChanged;
+        }
+
+        private void StepperNilai_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            entryNilai.Text = stepperNilai.Value.ToString();
+        }
+
+        private void BtnGetValue_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Keterangan", $"Usia Anda :{sliderUsia.Value}", "OK");
+        }
+    }
 }
