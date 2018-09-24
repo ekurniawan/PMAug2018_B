6b7b0176f2bfca0802760787475f35936609d266
erikur <erick@actual-training.com>
2018-09-24 13:14:34 +0700

    membuat ViewModel

---
+++ SampleAppKelasB/SampleAppKelasB/Models/Berita.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAppKelasB.Models
+{
+    public class Berita
+    {
+        public string Judul { get; set; }
+        public string Deskripsi { get; set; }
+    }
+}
---
+++ SampleAppKelasB/SampleAppKelasB/ViewModels/BeritaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using SampleAppKelasB.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SampleAppKelasB.ViewModels
+{
+    public class BeritaViewModel : BindableObject
+    {
+        public BeritaViewModel()
+        {
+            listBerita = new List<Berita>()
+            {
+                new Berita{Judul="Xamarin Forms",Deskripsi="Belajar Xamarin Forms dan Visual Studio"},
+                new Berita{Judul="CSharp Programming",Deskripsi="Belajar Pemrograman CSharp"},
+                new Berita{Judul="Xamarin Android",Deskripsi="Pengembangan Aplikasi Mobile dengan Xamarin for Android"}
+            };
+        }
+
+        private List<Berita> listBerita;
+        public List<Berita> ListBerita
+        {
+            get { return listBerita; }
+            set { listBerita = value; OnPropertyChanged("ListBerita"); }
+        }
+
+    }
+}
