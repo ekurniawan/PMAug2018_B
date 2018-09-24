4c5fc0c09d45fb2fb3151f9364eae6fe4468bded
erikur <erick@actual-training.com>
2018-09-24 13:51:14 +0700

    menambahkan navigation page

--- SampleAppKelasB/SampleAppKelasB/App.xaml.cs
+++ SampleAppKelasB/SampleAppKelasB/App.xaml.cs
@@ -11,7 +11,7 @@ namespace SampleAppKelasB
         {
             InitializeComponent();
 
-            MainPage = new TextListPage();
+            MainPage = new NavigationPage(new HomePage());
         }
 
         protected override void OnStart()
