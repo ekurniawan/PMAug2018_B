1d9f37c06d90ba18230a23dd571b80c2b9f6f62c
ekurniawan <erick.kurniawan@gmail.com>
2018-12-03 12:27:15 +0700

    menambahkan method getall

---
+++ SampleAppKelasB/SampleAppKelasB/Services/Helpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAppKelasB.Services
+{
+    public static class Helpers
+    {
+        public static string RestUrl = @"https://actservices.azurewebsites.net/api/TodoItem";
+    }
+}
---
+++ SampleAppKelasB/SampleAppKelasB/Services/TodoServices.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using SampleAppKelasB.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAppKelasB.Services
+{
+    public class TodoServices
+    {
+        private HttpClient _client;
+        public TodoServices()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<List<TodoItem>> GetAllData()
+        {
+            var items = new List<TodoItem>();
+            var uri = new Uri(Helpers.RestUrl);
+            try
+            {
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Kesalahan: {ex.Message}");
+            }
+            return items;
+        }
+    }
+}
